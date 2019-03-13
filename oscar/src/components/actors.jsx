d6bab9656e0e00f238b1de3b4dd333eb0cb1dd0a
unknown <mabbasidesign@yahoo.com>
2019-03-13 12:56:48 +0330

    best actors

--- oscar/src/components/actors.jsx
+++ oscar/src/components/actors.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import {ActorsList} from './data/actors';
+import { Link } from 'react-router-dom';
 
 const Actros = () => {
+
+    const actors = ActorsList.map((person) => {
+        return (
+            <div className="actor-container">
+                <a>
+                    {/* <img className="actor-image" src={person.img_src} /> */}
+                    <div className="actor-image" style={{ backgroundImage: "url(" + person.img_src + ")" }}></div>
+                </a>
+                <h3>{person.name}</h3>
+            </div>
+        )
+    })
+
     return ( 
-        <div>
-            Actros
+        <div className="main-content">
+        <div><Link className='back' to='/'>Back</Link></div>
+        <h2>Best Actors</h2>
+            <div className="container">
+                {actors}
+            </div>
         </div>
      );
 }
